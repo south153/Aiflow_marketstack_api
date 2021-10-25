2464f2a337107fee2c982f20eaae461fbeecc453
south153 <68831126+south153@users.noreply.github.com>
2021-10-25 11:29:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ try:
     from airflow import DAG
     from airflow.operators.python_operator import PythonOperator
     from datetime import datetime
-    import pandas as pd
+
 
     print("All Dag modules are ok ......")
 except Exception as e:
